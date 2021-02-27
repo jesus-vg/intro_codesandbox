134d2295e4201119d81c62ab312696c0b6011228
Jesús <51181686+jesusVillegasGonzalez@users.noreply.github.com>
2021-02-26 19:27:05 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  Pueba de codesandbox
+  <a href="https://jcodesoft.com" target="_blank">jcodesoft</a>.
+</div>
+`;
